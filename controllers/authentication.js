8a6a1d20304996039911048d8621bca7a5ff7ade
brtrduon <brianduongt@gmail.com>
2018-03-04 14:09:31 -0800

    rebuilt from the bottom up (again) minus the fancy shiznitz

---
+++ controllers/authentication.js
@@ -0,0 +1,45 @@
+const jwt = require('jwt-simple');
+const config = require('../config');
+const Admin = require('../models/kitchen');
+
+function token(admin) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({
+        sub: admin.id,
+        iat: timestamp
+        }, config.secret
+    );
+}
+
+exports.signin = function(req, res, next) {
+    res.send({ token: token(req.admin) });
+}
+
+exports.signup = function(req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || password) {
+        return releaseEvents.status(422).send({ error: 'Email and/or password cannot be blank'});
+    }
+    Admin.findOne({ username: username }, function(err, existingAdmin) {
+        if (err) {
+            return next(err);
+        }
+        if (existingAdmin) {
+            return res.status(422).send({ err: 'Username already in use'});
+        }
+        const admin = new Admin({
+            username: username,
+            first_name: first_name,
+            last_name: last_name,
+            password: password
+        });
+        admin.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.json({ token: token(admin) });
+        });
+    });
+}
---
+++ models/kitchen.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+const adminSchema = new Schema({
+    username: { type: String, unique: true, lowercase: true },
+    first_name: String,
+    last_name: String,
+    password: String
+});
+
+adminSchema.pre('save', function(next) {
+    const admin = this;
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(admin.password, salt, null, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+            admin.password = hash;
+            next();
+        });
+    });
+});
+
+adminSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, isMatch);
+    });
+}
+
+const modelClass = mongoose.model('admin', adminSchema);
+
+module.exports = modelClass;
